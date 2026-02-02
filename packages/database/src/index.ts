b36d8c47f3d1a4116b799a367df7172144aadb5e
Arin-Chandrakar <arin23official@gmail.com>
2026-02-02 23:54:22 +0530

    Tried fixing prisma client issue but not yet fixed fully

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 
 export const prismaClient = new PrismaClient({
     adapter:{url:process.env.DATABASE_URL}
