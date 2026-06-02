859fbc72058290ac21c89805310081017dd1d26f
Arin-Chandrakar <arin23official@gmail.com>
2026-06-02 09:25:21 +0530

    v1

--- apps/fe/app/page.tsx
+++ apps/fe/app/page.tsx
@@ -1,40 +1,55 @@
 "use client"
 
-import { easeInOut, motion } from "motion/react"
+import { easeInOut, motion, scale, transform } from "motion/react"
 import { useSpring } from "motion/react"
+import { MorphingText } from "@/components/ui/morphing-text"
 
 export default function Home() {
 
 
   return(
-      <div className=" flex flex-col bg-gradient-to-b from-blue-400 via-purple-200 to-transparent h-screen w-screen bg-[length:200%_200%] animate-gradient">
-        <div className=" flex justify-center items-center w-full h-60 sm:h-100 text-2xl sm:text-8xl font-inter font-bold">
-          <span>Welcome to&nbsp;</span>
-          <motion.span
-           className="font-metal text-3xl pt-1 sm:text-8xl sm:pt-3 decoration-wavy underline decoration-blue-600/80 underline-offset-9 will-change-transform"
-           animate={{y:[0,-20,0]}}
-           transition={{
-            duration:3,
-            repeat:Infinity,
-            repeatType:"mirror",
-            ease:"easeInOut",
-           }}
-           >Drawsome</motion.span>
-        </div>
-        <div className="flex w-full justify-center items-center">
-          <div className="relative overflow-hidden rounded-xl p-0.5">
-            <motion.div className="absolute -inset-full bg-[conic-gradient(from_60deg,transparent,transparent,#FF1493,transparent,blue)] z-0"
-               animate={{rotate:360}}
-               transition={{
-               duration: 3,
-               ease: "linear",
-               repeat: Infinity
-               }}>
-            </motion.div>
-            <div className="relative rounded-xl bg-black w-80">
-              <img src="/hanu.png" className="w-200 aspect-auto rounded-xl z-1" alt=""/>
-            </div>
+      <div className="flex flex-col bg-gradient-to-b pb-20 from-blue-400 via-purple-200  min-h-screen min-w-screen to-transparent bg-[length:200%_200%] animate-gradient">
+          <div className=" flex flex-col gap-4 justify-center items-center w-full h-60 mt-25 pt-20 sm:pt-10 sm:h-100 text-4xl sm:text-8xl font-inter font-bold">
+            <span>Welcome to&nbsp;</span>
+            <motion.span
+            className="font-metal text-6xl sm:text-8xl sm:pt-3 decoration-wavy underline decoration-blue-600/80 underline-offset-9 will-change-transform"
+            animate={{y:[0,-20,0]}}
+            transition={{
+              duration:3,
+              repeat:Infinity,
+              repeatType:"mirror",
+              ease:"easeInOut",
+            }}
+            >Drawsome
+            </motion.span>
+            <span className="text-4xl mt-10 text-[#1f0322] sm:pr-65 pr-35">
+              <MorphingText texts={["Think.", "Draw.","Share."]} />
+            </span>
+            <span className="text-xl max-w-3xl text-center mt-5 text-[#1f0322]">
+              A fast, lightweight whiteboard for teams, creators, and thinkers. Build diagrams, visualize systems, and communicate ideas with simple tools that stay out of your way and let your creativity lead.
+            </span>
           </div> 
+        <div className="flex w-full justify-center items-center pt-40">
+          <div className="relative">
+              <div className="relative overflow-hidden rounded-xl p-0.5">
+                <motion.div className="absolute -inset-full bg-[conic-gradient(from_60deg,transparent,transparent,#FF1493,transparent,blue)] z-0"
+                  animate={{rotate:360}}
+                  transition={{
+                  duration: 3,
+                  ease: "linear",
+                  repeat: Infinity
+                  }}
+                  >
+                </motion.div>
+                <div className=" flex relative rounded-xl bg-black w-80 sm:w-200">
+                  <img src="/hanu.png" className="w-200 aspect-auto rounded-xl z-1" alt=""/>
+                </div>
+              </div> 
+              <div className="absolute -left-72 -top-15 translate-x-30 rotate-90 mix-blend-exclusion">
+                <video src="/arrow.mp4" autoPlay loop muted className="h-30" ></video>
+              </div>
+          </div>
+          
         </div>
       </div>
   );
